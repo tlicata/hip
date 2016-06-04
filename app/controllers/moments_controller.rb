7f80869ac40956dddd2f8e67d9949f2cf5781698
Tim Licata <tim@timothylicata.com>
2016-06-03 21:16:32 -0700

    remove live updating routes and actions

--- app/controllers/moments_controller.rb
+++ app/controllers/moments_controller.rb
@@ -1,5 +1,4 @@
 class MomentsController < ApplicationController
-  before_action :set_moment, only: [:edit, :update, :destroy]
 
   # GET /moments
   # GET /moments.json
@@ -21,64 +20,4 @@ class MomentsController < ApplicationController
       end
     end
   end
-
-  # GET /moments/new
-  def new
-    @moment = Moment.new
-  end
-
-  # GET /moments/1/edit
-  def edit
-  end
-
-  # POST /moments
-  # POST /moments.json
-  def create
-    @moment = Moment.new(moment_params)
-
-    respond_to do |format|
-      if @moment.save
-        format.html { redirect_to @moment, notice: 'Moment was successfully created.' }
-        format.json { render :show, status: :created, location: @moment }
-      else
-        format.html { render :new }
-        format.json { render json: @moment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /moments/1
-  # PATCH/PUT /moments/1.json
-  def update
-    respond_to do |format|
-      if @moment.update(moment_params)
-        format.html { redirect_to @moment, notice: 'Moment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @moment }
-      else
-        format.html { render :edit }
-        format.json { render json: @moment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /moments/1
-  # DELETE /moments/1.json
-  def destroy
-    @moment.destroy
-    respond_to do |format|
-      format.html { redirect_to moments_url, notice: 'Moment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_moment
-      @moment = Moment.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def moment_params
-      params.require(:moment).permit(:name, :date, :info)
-    end
 end
