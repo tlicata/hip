0d9b8e94913583583eaae80432a0101d098dc276
Tim Licata <tim@timothylicata.com>
2016-08-27 07:26:30 -0700

    barilko's -> barilko

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ Moment.create([
   },
   {
     date: Date.new(1951, 8, 26),
-    name: "Bill Barilko's disappears",
+    name: "Bill Barilko disappears",
     info: "On August 26, 1951, Barilko joined his dentist, Henry Hudson, on a flight aboard Hudson's Fairchild 24 floatplane to Seal River, Quebec, for a weekend fishing trip. On the return trip, the single-engine plane disappeared and its passengers remained missing.",
     link: "he-was-on-a-fishing-trip"
   },
