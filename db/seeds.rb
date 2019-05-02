eb52cc8a2ef815bc3c5c445280c1e9d977b76b31
Tim Licata <tim@timothylicata.com>
2019-05-02 19:33:24 -0400

    add last concert moment

--- db/seeds.rb
+++ db/seeds.rb
@@ -132,5 +132,12 @@ Moment.create([
     date: Date.new(2016, 6, 17),
     name: "Man Machine Poem",
     link: "man-machine-poem"
+  },
+  {
+    date: Date.new(2016, 8, 20),
+    name: "Final Concert",
+    info: "The final concert of the Man Machine Poem tour was held at the Rogers K-Rock Centre in the band's hometown of Kingston on August 20, 2016. The concert was attended by Canadian prime minister Justin Trudeau. The concert was aired by the Canadian Broadcasting Corporation as a live cross-platform broadcast on CBC Television, CBC Radio One, CBC Radio 2, CBC Music, and YouTube. The concert featured 30 songs and three encore sets, with the band finishing with a performance of 'Ahead by a Century'. The CBC's broadcast and live streaming of the concert, uninterrupted by advertisements, was watched by 11.7 million people (roughly one-third of the Canadian population).",
+    link: "final-show",
+    tube: "fGWLtkCmbWE?start=9138"
   }
 ])
